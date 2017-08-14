e1f4460c79761aad980cc2502726295ea2604b0c
Graham Barber <grahammbarber@gmail.com>
2017-08-13 21:00:57 -0700

    Well, now I did

---
+++ main.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const https = require('https')
+
+https
+.get('https://itsthisforthat.com/api.php?json', res => {
+  res.on('data', d => {
+    Promise.resolve(JSON.parse(d)).then(simile => {
+      if (simile['this'].match(/(^A|^E|^I|^O|^U)/g)) {
+        console.log(
+          `So, basically, it's like an ${simile['this']} for ${simile['that']}!`
+        )
+      } else {
+        console.log(
+          `So, basically, it's like a ${simile['this']} for ${simile['that']}!`
+        )
+      }
+    })
+  })
+})
+.on('error', e => {
+  throw new Error(e)
+})
