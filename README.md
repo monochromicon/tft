67e373664ed8aa41bfad64ca45dedd9f09e4ac5d
Graham Barber <grahammbarber@gmail.com>
2017-08-13 20:31:00 -0700

    Initial commit?

---
+++ README.md
@@ -0,0 +1,16 @@
+# It's Like This for That
+
+A simple little CLI for talking to http://itsthisforthat.com. Just the thing
+when you need to feel inspired.
+
+```sh
+npm install -g tft
+```
+
+or, if you're fancy,
+
+```sh
+yarn
+```
+
+Full credit to [@erickerr](http://twitter.com/erickerr) and [@gilbert](http://twitter.com/gilbert), I'm just using their handy API.
---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const request = require('request')
+
+request.get('http://itsthisforthat.com/api.php?json', (err, res, body) => {
+  if (err) throw new Error(err)
+  Promise.resolve(JSON.parse(body)).then(simile => {
+    if (simile['this'].match(/(^A|^E|^I|^O|^U)/g)) {
+      console.log(
+        `So, basically, it's like an ${simile['this']} for ${simile['that']}!`
+      )
+    } else {
+      console.log(
+        `So, basically, it's like a ${simile['this']} for ${simile['that']}!`
+      )
+    }
+  })
+})
