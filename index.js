ec7d89f3ca16255743751c7ea3ad856160d99198
Graham Barber <grahammbarber@gmail.com>
2017-08-13 20:49:54 -0700

    Removed request, replaced with https

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 #!/usr/bin/env node
+const https = require('https')
 
-const request = require('request')
-
-request.get('http://itsthisforthat.com/api.php?json', (err, res, body) => {
-  if (err) throw new Error(err)
-  Promise.resolve(JSON.parse(body)).then(simile => {
-    if (simile['this'].match(/(^A|^E|^I|^O|^U)/g)) {
-      console.log(
-        `So, basically, it's like an ${simile['this']} for ${simile['that']}!`
-      )
-    } else {
-      console.log(
-        `So, basically, it's like a ${simile['this']} for ${simile['that']}!`
-      )
-    }
+https
+  .get('https://itsthisforthat.com/api.php?json', res => {
+    res.on('data', d => {
+      Promise.resolve(JSON.parse(d)).then(simile => {
+        if (simile['this'].match(/(^A|^E|^I|^O|^U)/g)) {
+          console.log(
+            `So, basically, it's like an ${simile['this']} for ${simile['that']}!`
+          )
+        } else {
+          console.log(
+            `So, basically, it's like a ${simile['this']} for ${simile['that']}!`
+          )
+        }
+      })
+    })
   })
-})
+  .on('error', e => {
+    throw new Error(e)
+  })
